e93bb22058f10a66dc7119761d03f4c2ee6822c3
Lucass9 <l_pisarek@o2.pl>
2022-07-27 08:10:57 +0200

    add background and nav

--- src/views/App.js
+++ src/views/App.js
@@ -2,22 +2,34 @@ import { GlobalStyle } from 'assets/Styles/GlobalStyle';
 import React from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import {theme} from '../assets/Styles/theme'
+import backgroud from '../assets/home/background-home-desktop.jpg'
+import Nav from 'components/organisms/navigation/Nav';
 
 const Wrapp = styled.div`
   display: flex;
-  align-items: center;
+  /* align-items: center; */
   justify-content: center;
   width: 100vw;
   height: 100vh;
+  /* background-image: url(${backgroud}); */
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
 `;
 
-function App() {
+const Img = styled.img ` 
+  position: absolute;
+  width: 100%;
+  z-index: -1;
+`
+const App = () => {
   return (
     <>
-      <GlobalStyle/>
       <ThemeProvider theme={theme}>
+      <GlobalStyle/>
         <Wrapp>
-          <h1>To jest nowa strona</h1>
+          <Nav/>
+          <Img src={backgroud}/>
         </Wrapp>
       </ThemeProvider>
     </>
