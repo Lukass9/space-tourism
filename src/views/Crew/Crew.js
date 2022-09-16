59eac2e4707094905f40c8a9a30f70db228f6d28
Lucass9 <l_pisarek@o2.pl>
2022-09-16 07:56:53 +0200

    add mobile viev to crew page

--- src/views/Crew/Crew.js
+++ src/views/Crew/Crew.js
@@ -1,10 +1,13 @@
 import React, {useEffect} from "react";
-import DestinationBg from 'assets/crew/background-crew-desktop.jpg'
+import desktop from 'assets/crew/background-crew-desktop.jpg'
+import tablet from 'assets/crew/background-crew-tablet.jpg'
+import mobile from 'assets/crew/background-crew-mobile.jpg'
 import { Outlet } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { H1, H1grey, NaviSub, PageTitle, Section1, Section2, StyledLinkSub, Wrapp } from "views/Destination/Destination.style";
 import styled from "styled-components";
 import { device } from "assets/Styles/theme";
+import { setBackground } from "helpers/hooks/setBackground";
 
 const Navi = styled(NaviSub)` 
     margin: 0;
@@ -47,7 +50,7 @@ const WrappForMobile = styled.div`
     flex-direction: column;
     width: 100vw;
     height:100%;
-    overflow-y: scroll;
+    overflow: hidden;
     align-items: center;
 
     @media ${device.desktop}{
@@ -55,11 +58,12 @@ const WrappForMobile = styled.div`
     }
 `
 
-const Crew = ({handleSetBackground, data}) =>{
+const Crew = ({handleSetBackground}) =>{
     const navigate = useNavigate();
 
     useEffect(()=>{
-        handleSetBackground(DestinationBg)
+        let background = setBackground(desktop, tablet, mobile)
+        handleSetBackground(background)
         navigate("/crew/douglas%20hurley", { replace: true })
     },[])
     return(
--- src/views/Crew/Persons.js
+++ src/views/Crew/Persons.js
@@ -1,42 +1,73 @@
+import { device } from "assets/Styles/theme";
 import React from "react";
 import styled from "styled-components";
 import { NaviSub, StyledLinkSub } from "views/Destination/Destination.style";
 
 const Img = styled.img`
-    /* min-height: 600px; */
+    max-height: 100%;
+
+    @media ${device.desktop}{
+        max-height: 120%;
+    }
 `
 const Section1 = styled.section`
     /* position: relative;
     top: 15%; */
-    justify-content: center;
     display: flex; 
+    justify-content: center;
+    align-items: center;
     flex-direction: column;
-    width: 43%;
+    width: 100%;
     /* min-height: 600px; */
     /* height: 100%;     */
+    @media ${device.desktop}{
+       align-items: flex-start;
+        width: 43%;
+    }
 `
 
 const Section2 = styled.section `
     display: flex;
     justify-content: center ;
     align-items: flex-end;
-    height: 69vh;
-    width: 32%;
+    height: 223px;
+    width: 85%;
+    order:-1;
+    border-bottom: 1px solid #383B4B;
+    margin-bottom: 20px;
+
+    @media ${device.desktop}{
+        order:1;
+        height: 69vh;
+        width: 32%;
+        align-items: flex-end;
+        border-bottom: none;
+        margin-bottom: auto;
+    }
 `
 const WrappContent = styled.div`
     display: flex;
     justify-content: center;
-    flex-direction: row;
+    align-items: center ;
+    flex-direction: column;
     width: 100%;
-    height: 81.9%;
+
+    @media ${device .desktop}{
+        flex-direction: row;
+        height: 81.9%;
+    }
 `
 const Role = styled.h1`
     font-family: 'Bellefair';
     font-weight: 400;
-    font-size: 32px;
+    font-size: 16px;
     text-transform: uppercase;
     color: #FFFFFF;
     opacity: 0.5;
+
+    @media ${device.desktop}{
+        font-size: 32px;
+    }
 `
 const Name = styled.h1`
     margin: 0;
@@ -45,30 +76,48 @@ const Name = styled.h1`
     font-family: 'Bellefair';
     font-style: normal;
     font-weight: 400;
-    font-size: 56px;
+    font-size: 24px;
     text-transform: uppercase;
     color: #FFFFFF;
+
+    @media ${device.desktop}{
+        font-size: 56px;
+    }
 `
 const Description = styled.h1`
     margin-top: 8%;
     font-family: 'Barlow';
     font-style: normal;
     font-weight: 400;
-    font-size: 18px;
-    line-height: 32px;
+    font-size: 15px;
+    line-height: 25px;
     max-width: 442px;
     min-height: 160px;
     color: #D0D6F9;
+    width: 85%;
+    text-align: center;
+
+    @media ${device.desktop}{
+        font-size: 18px;
+        width: auto;
+        text-align: left;
+        line-height: 32px;
+    }
 `
 const Navi = styled(NaviSub)` 
     margin: 0;
     transform:translateY(0);
     width: auto;
+    order: -1;
+
+    @media ${device.desktop}{
+        order: 1;
+    }
 `
 const activeClassName = 'active';
 const StyledDotLink = styled(StyledLinkSub)` 
-    width: 13px;
-    height: 13px;
+    width: 10px;
+    height: 10px;
     background-color: #FFF;
     border-radius: 50%;
     margin: 12px ;
@@ -87,6 +136,11 @@ const StyledDotLink = styled(StyledLinkSub)`
         &::after{
             content: none;
         }
+    }
+
+    @media ${device.desktop}{
+        width: 13px;
+        height: 13px;
     }
 
 `
